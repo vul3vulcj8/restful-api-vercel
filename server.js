1a07191da9c88abb2660fdf597e1d7cfc50cd46d
Claire <29aulaul3@gmail.com>
2023-12-30 13:41:49 +0800

    finish

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+// JSON Server module
 import jsonServer from "json-server";
-import cors from "cors";
-// 創建 json-server 實例
-
 const server = jsonServer.create();
-import router from "db/db.json";
+const router = jsonServer.router("db/db.json");
 
-// 使用 cors 中间件
-server.use(cors());
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
+// Add this before server.use(router)
 server.use(
+  // Add custom route here if needed
   jsonServer.rewriter({
     "/api/*": "/$1",
   })
 );
 server.use(router);
-
+// Listen to port
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
 
+// Export the Server API
 module.exports = server;
