a48253943c4dc2e712a0886d7c3edc193cbf7a60
Claire <29aulaul3@gmail.com>
2023-12-30 18:52:31 +0800

    finish

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = jsonServer.router("db/db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
-server.use(jsonServer.delay(1500));
+
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
